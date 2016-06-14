335f874cf4d56d59cbd35402ecdc063e05624517
Thomas Weise <tweise@ustc.edu.cn>
2016-06-14 16:48:48 +0800

    DoubleConstantParameters can now use NumberAppender for Number Rendering

--- src/main/java/org/optimizationBenchmarking/utils/math/text/DoubleConstantParameters.java
+++ src/main/java/org/optimizationBenchmarking/utils/math/text/DoubleConstantParameters.java
@@ -2,6 +2,8 @@ package org.optimizationBenchmarking.utils.math.text;
 
 import org.optimizationBenchmarking.utils.document.spec.IMath;
 import org.optimizationBenchmarking.utils.document.spec.IText;
+import org.optimizationBenchmarking.utils.text.ETextCase;
+import org.optimizationBenchmarking.utils.text.numbers.NumberAppender;
 import org.optimizationBenchmarking.utils.text.textOutput.ITextOutput;
 
 /**
@@ -10,6 +12,8 @@ import org.optimizationBenchmarking.utils.text.textOutput.ITextOutput;
 public final class DoubleConstantParameters
     extends AbstractParameterRenderer {
 
+  /** the number appender to use */
+  private final NumberAppender m_appender;
   /** the parameter values */
   private final double[] m_parameters;
 
@@ -20,12 +24,26 @@ public final class DoubleConstantParameters
    *          the constant parameter values
    */
   public DoubleConstantParameters(final double[] parameters) {
+    this(null, parameters);
+  }
+
+  /**
+   * Create the double constant parameter renderer
+   *
+   * @param appender
+   *          the number appender to use
+   * @param parameters
+   *          the constant parameter values
+   */
+  public DoubleConstantParameters(final NumberAppender appender,
+      final double[] parameters) {
     super();
     if (parameters == null) {
       throw new IllegalArgumentException(
           "Parameter array must not be null."); //$NON-NLS-1$
     }
     this.m_parameters = parameters;
+    this.m_appender = appender;
   }
 
   /** {@inheritDoc} */
@@ -33,7 +51,12 @@ public final class DoubleConstantParameters
   public final void renderParameter(final int index,
       final ITextOutput out) {
     if ((index >= 0) && (index < this.m_parameters.length)) {
-      out.append(this.m_parameters[index]);
+      if (this.m_appender == null) {
+        out.append(this.m_parameters[index]);
+      } else {
+        this.m_appender.appendTo(this.m_parameters[index],
+            ETextCase.IN_SENTENCE, out);
+      }
     } else {
       AbstractParameterRenderer.throwInvalidParameterIndex(index,
           this.m_parameters.length);
@@ -45,7 +68,12 @@ public final class DoubleConstantParameters
   public final void renderParameter(final int index, final IMath out) {
     if ((index >= 0) && (index < this.m_parameters.length)) {
       try (final IText text = out.number()) {
-        text.append(this.m_parameters[index]);
+        if (this.m_appender == null) {
+          text.append(this.m_parameters[index]);
+        } else {
+          this.m_appender.appendTo(this.m_parameters[index],
+              ETextCase.IN_SENTENCE, text);
+        }
       }
     } else {
       AbstractParameterRenderer.throwInvalidParameterIndex(index,
